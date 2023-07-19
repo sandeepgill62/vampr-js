6047e813d0daa43db5192b27b4ac90c1d327c3d3
Sandeep Gill <sandeepgill62@gmail.com>
2023-07-19 00:33:31 +0000

    adding vampire class

--- vampire.js
+++ vampire.js
@@ -9,25 +9,36 @@ class Vampire {
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
-
+  addOffspring (vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
-
+  get numberOfOffspring () {
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
+  get numberOfVampiresFromOriginal () {
+    let numberOfVampiresAwayFromOriginal = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampiresAwayFromOriginal++;
+    }
+    return numberOfVampiresAwayFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
+  isMoreSeniorThan (vampire) {
+    if (this.offspring.length > vampire.offspring.length) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -35,7 +46,7 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  closestCommonAncestor (vampire) {
 
   }
 }
